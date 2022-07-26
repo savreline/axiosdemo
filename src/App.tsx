c89a5de4cb3e73ed7ef87666fe95fe86dfd13954
Sasha Avreline <sasha.avreline@usask.ca>
2022-07-26 11:49:28 -0700

    Add file upload field

--- src/App.tsx
+++ src/App.tsx
@@ -18,10 +18,37 @@ const App = () => {
       );
   };
 
+  const handleSubmitFile = (event: any) => {
+    const image: File = event.currentTarget.files[0];
+    console.log(image);
+    var formData = new FormData();
+    formData.append("image", image);
+    axios
+      .post("/api/sample", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((uploadResponse: AxiosResponse | Promise<AxiosResponse>) =>
+        console.log(uploadResponse)
+      );
+
+    const element = window.document.getElementById(
+      "fileField"
+    ) as HTMLInputElement;
+    if (element !== null) {
+      element.value = "";
+    }
+  };
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input {...register("field1")} />
+      <br />
       <input {...register("field2")} />
+      <br />
+      <input type="file" id="fileField" onChange={handleSubmitFile} />
+      <br />
       <input type="submit" />
     </form>
   );
