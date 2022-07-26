774014031a14a18c8404eea28da18b9ae2d0c6f4
Sasha Avreline <sasha.avreline@usask.ca>
2022-07-26 11:42:55 -0700

    Add backend call using application/x-www-form-urlencoded

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import axios, { AxiosResponse } from "axios";
 import React from "react";
 import { useForm } from "react-hook-form";
 
 const App = () => {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data: any) => console.log(data);
+  const onSubmit = (data: any) => {
+    console.log(data);
+    axios
+      .post("/api/sample", data, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      })
+      .then((uploadResponse: AxiosResponse | Promise<AxiosResponse>) =>
+        console.log(uploadResponse)
+      );
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
